120655b552e4e9123cff5b69c41eaeed333f0641
svipas <benas.svipas@gmail.com>
2020-04-30 19:05:57 +0300

    Rename main.test.ts to extension.test.ts

---
+++ src/test/suite/extension.test.ts
@@ -0,0 +1,18 @@
+import * as assert from 'assert';
+import { ExtensionData, getAllExtensionsFromVSCode, getExtensionIdFromText } from '../../extension';
+
+test('getAllExtensionsFromVSCode()', async () => {
+	const extensions = await getAllExtensionsFromVSCode();
+	extensions.all.forEach((ext) => {
+		const isAllValuesDefined = Object.keys(ext).every((key) => ext[key as keyof ExtensionData] != null);
+		assert(isAllValuesDefined, 'missing properties');
+	});
+});
+
+test('getExtensionIdFromText()', () => {
+	const builtin = 'vscode.bat (built-in) 🔋';
+	assert.strictEqual(getExtensionIdFromText(builtin), 'vscode.bat');
+
+	const installed = 'ms-vscode.Go (installed) 🔌';
+	assert.strictEqual(getExtensionIdFromText(installed), 'ms-vscode.Go');
+});
